b096b234ef8593255c182178f0fcca83ac7bd507
DjordjeStojanovic <djordje@heavyform.com>
2017-05-09 16:37:24 +0200

    run build.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
-var Site = function() {
+var Device = (function() {
+
+    var exp = {
+        get: function() {
+            if (Utility.check.phone()) {
+                return 'phone';
+            } else if (Utility.check.tablet()) {
+                return 'tablet';
+            } else {
+                return 'desktop';
+            }
+        },
+        check: function() {
+            var device = exp.get();
+            if (device == 'phone') {
+                $('body').removeClass('desktop').addClass('phone');
+                exp.adjustViewport(640);
+            } else if (device == 'tablet') {
+                $('body').removeClass('desktop').addClass('tabket');
+                exp.adjustViewport(768);
+            }
+        },
+        adjustViewport: function(width) {
+            $('meta[name=viewport]').attr('content', 'width=' + width + ', user-scalable=no');
+        }
+    }
+
+    return exp;
+
+})();
+
+var Site = function(Device) {
     var exp = {
         router: function() {
-
             Hashbang.map('/', function() {
                 
             });
@@ -10,6 +40,7 @@ var Site = function() {
 
     function init() {
         exp.router();
+        Device.check();
     }
     init();
 
@@ -21,5 +52,5 @@ var Site = function() {
 
 // init site after dom ready
 $(document).ready(function() {
-    var site = Site();
+    var site = Site(Device);
 });
